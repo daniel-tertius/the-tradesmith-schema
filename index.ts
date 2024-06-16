4c838459f4c38b32451cd4470795ad0f42792033
Tertius van Niekerk <tertius.vanniekerk@protonmail.com>
2024-06-16 12:43:51 +0200

    Updated Schema

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
+import { ObjectId } from "mongodb";
+
 export default interface Schema {
-  log: log;
-  config: config;
+  // log: log;
+  bot: bot;
   user: user;
 }
 
-interface log {
-  created_at: Date;
-  title: string;
-  message: string;
-  actor: string;
-  action: string;
-  success: boolean;
+// interface log {
+//   created_at: Date;
+//   title: string;
+//   message: string;
+//   actor: string;
+//   action: string;
+//   success: boolean;
+// }
+
+interface bot {
+  _id: ObjectId;
+  bot_title: string;
+  user_id: string;
 }
 
-interface config {}
-
 interface user {
+  _id: ObjectId;
   email_address: string;
   password: string;
   last_logged_in: Date;
